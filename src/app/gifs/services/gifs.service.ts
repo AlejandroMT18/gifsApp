14768e97104d9013d6de11e1ba78f94287634616
Alejandro <alejandro.redes18@gmail.com>
2021-08-19 15:36:38 -0600

    Primer Commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+
+  private _historial: string[] = [];
+  private apiKey: string = "4cdmvl9SYaMAj3CNvagYSPIUn0U9gAtM";
+  
+  public resultados: any[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient ){}
+
+  buscarGifs( query: string ){
+    if( !this._historial.includes( query )){
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+    }
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=4cdmvl9SYaMAj3CNvagYSPIUn0U9gAtM&q=${query}&limit=10`)
+     .subscribe( (response:any) => {
+       console.log( response.data );
+       this.resultados = response.data;
+     })
+
+  }
+
+}
